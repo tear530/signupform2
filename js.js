364134651ea293c859b2b3d9b830f44e0c28ba9e
tear530 <99728549+tear530@users.noreply.github.com>
2022-07-13 16:31:49 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,85 +1,148 @@
-const pass1 = document.querySelector("#user_password1");
-const pass2 = document.querySelector("#user_password2");
-const fname = document.querySelector("#first_name");
-const fnameV = document.querySelector(".nameVal");
-const snameV = document.querySelector(".sNameVal");
-const emailval = document.querySelector(".emailval");
-const phoneVal = document.querySelector(".phoneVal");
-const sname = document.querySelector("#last_name");
-const email = document.querySelector("#user_email");
-const phone = document.querySelector("#amount");
-const para = document.querySelector(".pval");
-const para2 = document.querySelector(".pval2");
-const borderError1 = document.querySelector(".error1");
-const borderError2 = document.querySelector(".error2");
-const btn = document.querySelector(".submit");
-btn.disabled = true;
-const regex = /\S+@\S+\.\S+/;
-const regex2 = /^[0-9]{11}$/;
+const btn = document.querySelector('#but');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('#read');
+const cards = document.querySelector('.cards');
+const card = document.querySelector('.card');
+const form = document.querySelector('.form1');
 
 
-document.addEventListener("keyup", e => {
-    if (fname.value !== '') {
-        fnameV.textContent = '✓Valid'
-        fnameV.style.color = "green";
 
-    } if (sname.value !== '') {
-        snameV.textContent = '✓Valid'
-        snameV.style.color = "green";
+let myLibrary = [];
 
-    } if (fname.value == '') {
-        fnameV.textContent = '*Must be filled'
-        fnameV.style.color = "red";
+function Book(title, author, pages, readStatus, index) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus
+    this.index = index;
 
-    } if (sname.value == '') {
-        snameV.textContent = '*Must be filled'
-        snameV.style.color = "red";
-
-    } if ((regex.test(email.value)) == true) {
-        emailval.textContent = '✓Valid'
-        emailval.style.color = "green";
-
-    } if ((regex.test(email.value)) == false) {
-        emailval.textContent = '*Incorrect format'
-        emailval.style.color = "red";
-
-    } if ((regex2.test(phone.value)) == false) {
-        phoneVal.textContent = '*Must be 11 digits no spaces'
-        phoneVal.style.color = "red";  
-
-    } if ((regex2.test(phone.value)) == true) {
-        phoneVal.textContent = '✓Valid'
-        phoneVal.style.color = "green";
-
-    } if ((pass1.value !== pass2.value)) {
-        para.textContent = '*Passwords do not match'
-        para.style.color = "red";
-        borderError1.style.border = "1px solid red";
-        borderError2.style.border = "1px solid red";
-        //btn.disabled = true;
-
-    } if (pass1.value == pass2.value && (pass1.value || pass2.value !== '')) {
-        para.textContent = '✓Passwords match'
-        para.style.color = "green";
-        para2.textContent = '✓Passwords match'
-        para2.style.color = "green";
-        borderError1.style.border = "1px solid green";
-        borderError2.style.border = "1px solid green";
+}
 
 
-    } if ((pass1.value == pass2.value) && (pass1.value || pass2.value !== '') && (fname.value !== '') && (sname.value !== '') && (regex2.test(phone.value)) == true && ((regex.test(email.value)) == true)) {
-        btn.disabled = false;
-        
-    }
+
+function addBookToLibrary(title, author, pages, readStatus, index) {
+    let book = new Book(title, author, pages, readStatus, index);
+    myLibrary.push(book);
+}
+
+
+
+
+btn.addEventListener('click', () => {
+
+    let a = (title.value);
+    let b = (author.value);
+    let c = (pages.value);
+    let d = (read.checked);
+    let index = (myLibrary.length);
+
+
+    addBookToLibrary(a, b, c, d, index);
+    appendData(a, b, c, d, index);
+    form.reset();
+
+
 });
 
+function displayOnScreen() {
+    myLibrary.forEach((val, index) => {
 
-// \S+@\S+\.\S+
+        appendData(val.title, val.author, val.pages, val.readStatus, index);
+
+
+    });
+
+}
+
+function appendData(a, b, c, d, ind) {
+
+    const el = document.createElement('div');
+    el.classList.add('card');
+    cards.appendChild(el);
+
+    const para = document.createElement('p');
+    para.classList.add('titles');
+    para.textContent = `Title: ${a}`
+    el.appendChild(para);
+
+    const para1 = document.createElement('p');
+    para1.classList.add('titles');
+    para1.textContent = `Author: ${b}`
+    el.appendChild(para1);
+
+    const para2 = document.createElement('p');
+    para2.classList.add('titles');
+    para2.textContent = `Pages: ${c}`
+    el.appendChild(para2);
+
+    const para3 = document.createElement("BUTTON");
+
+    if (d == "true" || d == true) {
+
+        para3.classList.add('titles', 'read', "bts");
+        para3.textContent = "Read";
+
+    } if (d == "false" || d == false) {
+        para3.classList.add('titles', 'noread', "bts");
+        para3.textContent = "Unread";
+    }
+
+    el.appendChild(para3);
+
+    para3.setAttribute('id', "b" + ind);
 
 
 
+    const imgs = document.createElement('img');
+    imgs.src = "/trashcan.png";
+    imgs.classList.add('imgs');
+    el.appendChild(imgs);
 
 
+    imgs.setAttribute('id', ind);
+    el.setAttribute('id', "div" + ind);
+
+
+
+    document.getElementById(ind).addEventListener('click', () => {
+        myLibrary.splice(ind, 1);
+        reorderArray();
+    });
+
+    function reorderArray() {
+        document.getElementById("div" + ind).remove();
+        document.querySelectorAll('.card').forEach(card => cards.removeChild(card));
+        displayOnScreen();
+
+    }
+
+    document.getElementById("b" + ind).addEventListener('click', () => {
+
+        if (para3.textContent == "Unread") {
+
+            para3.textContent = "Read";
+            para3.classList.replace('noread', 'read');
+
+        } else if (para3.textContent == "Read") {
+
+            para3.textContent = "Unread"
+            para3.classList.replace('read', 'noread');
+        }
+
+
+    });
+
+
+
+}
+
+
+addBookToLibrary("Harry Potter", "JK Rowling", "88", "true");
+addBookToLibrary("Harry Potter2", "JK Rowling2", "88", "false");
+
+displayOnScreen();
 
 
 
